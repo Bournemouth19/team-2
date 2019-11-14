bfe716bba0e2dc2769260a46e23f4236ff842dbe
root <alexander.petrov420@gmail.com>
2019-11-14 14:21:21 +0200

    0.3

--- Server.js
+++ Server.js
@@ -1,15 +1,47 @@
 var express = require('express');
+var app = express()
 var fs = require('fs');
+
+app.listen(80,listening);
+
+app.get('/video', function(req, res) {
+    const path = 'assets/sample.mp4'
+    const stat = fs.statSync(path)
+    const fileSize = stat.size
+    const range = req.headers.range
+    if (range) {
+      const parts = range.replace(/bytes=/, "").split("-")
+      const start = parseInt(parts[0], 10)
+      const end = parts[1] 
+        ? parseInt(parts[1], 10)
+        : fileSize-1
+      const chunksize = (end-start)+1
+      const file = fs.createReadStream(path, {start, end})
+      const head = {
+        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+        'Accept-Ranges': 'bytes',
+        'Content-Length': chunksize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(206, head);
+      file.pipe(res);
+    } else {
+      const head = {
+        'Content-Length': fileSize,
+        'Content-Type': 'video/mp4',
+      }
+      res.writeHead(200, head)
+      fs.createReadStream(path).pipe(res)
+    }
+  });
+ 
 
 
 
 function listening(){
 
-    console.log("Http Listening...")
+    console.log("Listening... port 80")
   
 }
-
-httpServer.listen(80,listening);
-
 
 app.use(express.static("Home_page"));
