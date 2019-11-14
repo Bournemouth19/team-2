6a17dec06cfd47388b15d027cf203111cd3add17
paulmccann24 <36445072+paulmccann24@users.noreply.github.com>
2019-11-14 12:03:44 +0000

    Add files via upload
    User database

---
+++ users.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `users` (
+  `userid` bigint(20) NOT NULL,
+  `Username` varchar(50) NOT NULL,
+  `Password` varchar(50) NOT NULL,
+  `E-mail` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`userid`);
+
+
+ALTER TABLE `users`
+  MODIFY `userid` bigint(20) NOT NULL AUTO_INCREMENT;
+
